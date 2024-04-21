9a91aed0fcce0a4a0630dd3f6c0812d55bbf0c33
ritika <kumar.riti@northeastern.edu>
2024-04-21 13:49:08 -0400

    App changes, requirements changes, add method to eval model

--- main.py
+++ main.py
@@ -3,19 +3,25 @@ Program to demonstrate usage of whowrotethis
 python module
 """
 
-from whowrotethis import TextPreprocessing, TextEmbedding, Classifier
+from whowrotethis import TextPreprocessing, TextEmbedding, Classifier, UserApp
 # import whowrotethis
 
 
 def main():
+    # For text preprocessing
     text = "This is a test."
     processor = TextPreprocessing(text, file_given=False)
     preprocessed_text = processor.preprocess()
     print(preprocessed_text)
 
+    # For text embeddings
     embeddings = TextEmbedding('text.txt').get_embeddings()
     predict = Classifier(embeddings)
     print(predict.predict_text())
+
+    # for streamlit app (uncomment below)
+    # UserApp().run_app()
+
 
 
 if __name__ == "__main__":
