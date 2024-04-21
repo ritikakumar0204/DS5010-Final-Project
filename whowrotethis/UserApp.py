53b3cc905a4931fc31611f0d165f0f984129eecb
Ardavan <139175955+ardavano@users.noreply.github.com>
2024-04-21 16:15:16 -0400

    Edits Comments and Docstrings

--- whowrotethis/UserApp.py
+++ whowrotethis/UserApp.py
@@ -1,19 +1,30 @@
 """
+A Python module for launching a Streamlit application.
 
-Python program to run streamlit app whowrotethis_app.py
+Using UserApp class to start the Streamlit application.
+The class is designed to execute a shell command that launches the application on localhost.
+
 """
 import os
 
 
 class UserApp:
     """
-    class to run streamlit app
+    Class to run Streamlit app
     method:
-    run_app() : opens app in localhost
+    run_app(): Executes the command stored in self.run_command to start the application and
+    opens app in localhost.
     """
 
     def __init__(self):
         self.run_command = "streamlit run whowrotethis_app.py"
 
     def run_app(self):
+        """
+            Launches the Streamlit application
+
+            This method uses the os.system function to call the command line and execute the Streamlit run command.
+            It effectively starts the server and hosts the application on localhost accessible via a web browser.
+        """
+        # Execute the command to run the application
         os.system("streamlit run whowrotethis//whowrotethis_app.py")
